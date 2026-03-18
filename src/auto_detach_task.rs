65bf4d91a1644a040774c5a447b7c7f9fc9f0426
Paul Saunders <coruscateor@users.noreply.github.com>
2026-03-18 19:50:58 +1300

    - Updated the package version string to "0.2.0-alpha".
    - Updated the act_rs dependency to point to a local repository.
    
    - Added the async-trait feature.
    
    - Made made the presence of the TaskActor struct dependant on the newly added async-trait feature.
    
    - Added the AutoDetachTask

---
+++ src/auto_detach_task.rs
@@ -0,0 +1,65 @@
+use std::ops::Deref;
+
+use smol::Task;
+
+pub struct AutoDetachTask<T, M = ()>
+{
+
+    task: Option<Task<T, M>>
+
+}
+
+impl<T, M> AutoDetachTask<T, M>
+{
+
+    pub fn new(task: Task<T, M>) -> Self
+    {
+
+        Self
+        {
+
+            task: Some(task)
+
+        }
+
+    }
+
+    pub fn take(mut self) -> Task<T, M>
+    {
+
+        self.task.take().expect("Task must be present.")
+
+    }
+
+}
+
+impl<T, M> Deref for AutoDetachTask<T, M>
+{
+
+    type Target = Task<T, M>;
+
+    fn deref(&self) -> &Self::Target
+    {
+
+        self.task.as_ref().expect("Task must be present.")
+
+    }
+
+}
+
+impl<T, M> Drop for AutoDetachTask<T, M>
+{
+
+    fn drop(&mut self)
+    {
+
+        if let Some(task) = self.task.take()
+        {
+
+            task.detach();
+
+        }
+
+    }
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,19 @@
 
 //#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
+#[cfg(feature = "async-trait")]
 mod task_actor;
 
+#[cfg(feature = "async-trait")]
 pub use task_actor::*;
 
 mod mac_task_actors;
 
 pub use mac_task_actors::*;
+
+mod auto_detach_task;
+
+pub use auto_detach_task::*;
 
 /*
 pub fn add(left: u64, right: u64) -> u64 {
